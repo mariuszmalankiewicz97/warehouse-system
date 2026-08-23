2dc97a4aade6431273e2b95b31ba86e42a081265
Mariusz <mariuszmalankiewicz@gmail.com>
2026-08-23 14:01:13 +0200

    refactor: update add product to stock

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 stock = []
 
 while True:
-    name = input("Name product: ")
-    price = input("Price product: ")
-    amount = input("Amount product: ")
-    product = {}
-    product[name] = {price, amount}
-    stock.append(product)
-    print(stock)
+    menu = int(input("""Werehouse Menu:
+1) Add product
+9) Exit 
+Your choose: """))
+    if menu == 1:
+        name = input("Name product: ")
+        price = input("Price product: ")
+        amount = input("Amount product: ")
+        product = {}
+        product[name] = {price, amount}
+        stock.append(product)
+        print(stock)
+    if menu == 9:
+        break
