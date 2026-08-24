d421122375be9c91de8a82fa69fc2832ac42b9c5
Mariusz <mariuszmalankiewicz@gmail.com>
2026-08-24 09:34:10 +0200

    feat: add delete product

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ while True:
     menu = int(input("""Werehouse Menu:
 1) Add product
 2) View products
+3) Del product
 9) Exit 
 Your choose: """))
     if menu == 1:
@@ -17,5 +18,11 @@ Your choose: """))
     if menu == 2:
         for product in stock:
             print(f"Product: {product}")
+    if menu == 3:
+        name = input("Name product to delete: ")
+        for product in stock:
+            if product in stock:
+                del product[name]
+                print(f"\nProduct delete success: {name}\n")
     if menu == 9:
         break
