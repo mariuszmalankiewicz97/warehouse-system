f0eda674888eb6eb359c0ebd7d91d6e5dbecd8ec
Mariusz <mariuszmalankiewicz@gmail.com>
2026-08-24 10:36:02 +0200

    feat: add update amount

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ while True:
 1) Add product
 2) View products
 3) Del product
+4) Change amount product
 9) Exit 
 Your choose: """))
     if menu == 1:
@@ -12,7 +13,7 @@ Your choose: """))
         price = input("Price product: ")
         amount = input("Amount product: ")
         product = {}
-        product[name] = {price, amount}
+        product[name] = {"price": price, "amount": amount}
         stock.append(product)
         print(f"Product add success: {product}")
     if menu == 2:
@@ -24,5 +25,16 @@ Your choose: """))
             if product in stock:
                 del product[name]
                 print(f"\nProduct delete success: {name}\n")
+    if menu == 4:
+        name = input("\nname product: ")
+        for product in stock:
+            if product in stock:
+                current_amount = product[name]["amount"]
+                print(f"\nCurrent amount: {current_amount} \n")
+                new_amount = input("New amount: ")
+                product[name] = {
+                    "price": product[name]["price"],
+                    "amount": new_amount,
+                }
     if menu == 9:
         break
